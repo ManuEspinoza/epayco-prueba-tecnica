5958b524847ffd6a4b30554eac1aff28eca69c96
manuEspinoza <manuel.espinoza1996@gmail.com>
2020-11-04 22:21:23 -0400

    Agregar fondos a la cartera virtual

--- backend/src/Client/client.controller.js
+++ backend/src/Client/client.controller.js
@@ -30,7 +30,34 @@ const registerClient = async (req, res, next) => {
 }
 
 const addClientBalance = async (req, res, next) => {
-    res.send("agregar fondos");
+    try {
+        const { document, phone, balance } = req.body;
+        if (document === undefined || phone === undefined || balance === undefined) {
+            throw new AppError('Todos los campos son obligatorios', 400, 400);
+        }
+        if (validator.isEmpty(phone)) {
+            throw new AppError('Todos los campos son obligatorios', 400, 400);
+        }
+        if (!validator.isMobilePhone(phone)) {
+            throw new AppError('Numero de telefono invalido', 400, 400);
+        }
+        if (document <= 0) {
+            throw new AppError('Identificacion invalida', 400, 400);
+        }
+        if (balance <= 0) {
+            throw new AppError('El balance no puede ser 0 o un numero negativo', 400, 400);
+        }
+        const client = await Client.findOne({ document, phone });
+        if (!client) {
+            throw new AppError('Credenciales invalidas, por favor verifique e intente nuevamente', 404, 400);
+        }
+
+        client.balance = client.balance + balance;
+        await client.save();
+        res.status(200).send(client);
+    } catch (e) {
+        next(e);
+    }
 }
 
 const getClientBalance = async (req, res, next) => {
